d7d9feb7a66f63af15ec7467dd1700f8c6570ece
sultampan <sultansyarif630@gmail.com>
2024-09-07 10:06:16 +0800

    Tugas Biodata Kelompok

---
+++ BiodataSultan.java
@@ -0,0 +1,12 @@
+// BiodataNama.java
+public class BiodataSultan {
+    public static void main(String[] args) {
+        String nama = "Sultan";
+        String nim = "244107060151"; //menggunakan tipedata String
+        String hobi = "Membaca buku";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("nim: " + nim);
+        System.out.println("Hobi: " + hobi);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Proyek Biodata Kelompok
+
+Ini adalah proyek dari kelompok untuk menampilkan biodata setiap anggota tim. 
+Setiap anggota menambahkan program yang menampilkan informasi pribadi mereka.
